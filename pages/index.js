ddfe1db8272a904eeb2cf0779f2c7e647fc06583
kasir1978 <32015948+kasir1978@users.noreply.github.com>
2025-02-23 17:09:04 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Editor Literario</h1>
+      <p>Corrige, mejora y analiza tus textos con inteligencia artificial.</p>
+    </div>
+  );
+}
